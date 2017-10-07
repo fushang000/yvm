9f8bba8a905694eab9748edee0705e632c99d624
Cthulhu <1948638989@qq.com>
2017-10-07 11:57:45 +0800

    infrastructure: implement red black tree for further basic utility data structure

--- infrastructure/main.cpp
+++ infrastructure/main.cpp
@@ -1,10 +1,11 @@
 #include <iostream>
-#include "../classfile/Common.h"
-
-void fun(ssvm::u1 byte){
-}
+#include "RBTree.hpp"
 
 int main(int argc, char *args[]){
-
+    RBTree<unsigned int, unsigned int> tree;
+    for (size_t i = 0; i < 5000000; i++){
+        tree.put(i, i);
+        tree.remove(i);
+    }
     return 0;
 }
